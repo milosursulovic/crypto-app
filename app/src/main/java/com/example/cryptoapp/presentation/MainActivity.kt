0f9b05197fe4f1d009013110355eee0f0f0e86f8
Misa <milosursulovic2696@gmail.com>
2022-11-13 19:33:04 +0100

    Add navigation

--- app/src/main/java/com/example/cryptoapp/presentation/MainActivity.kt
+++ app/src/main/java/com/example/cryptoapp/presentation/MainActivity.kt
@@ -3,6 +3,11 @@ package com.example.cryptoapp.presentation
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.cryptoapp.presentation.coin_detail.CoinDetailScreen
+import com.example.cryptoapp.presentation.coin_list.CoinListScreen
 import com.example.cryptoapp.presentation.ui.theme.CryptoAppTheme
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -12,7 +17,18 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             CryptoAppTheme {
-
+                val navController = rememberNavController()
+                NavHost(
+                    navController = navController,
+                    startDestination = Screen.CoinListScreen.route
+                ) {
+                    composable(route = Screen.CoinListScreen.route) {
+                        CoinListScreen(navController = navController)
+                    }
+                    composable(route = Screen.CoinDetailScreen.route + "/{coinId}") {
+                        CoinDetailScreen()
+                    }
+                }
             }
         }
     }

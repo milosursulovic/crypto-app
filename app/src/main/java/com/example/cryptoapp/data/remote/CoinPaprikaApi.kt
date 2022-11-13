5678210f16fd92579e14b70167cf6e14d63c415f
Misa <milosursulovic2696@gmail.com>
2022-11-13 14:31:32 +0100

    Add api service and dtos

---
+++ app/src/main/java/com/example/cryptoapp/data/remote/CoinPaprikaApi.kt
@@ -0,0 +1,13 @@
+package com.example.cryptoapp.data.remote
+
+import com.example.cryptoapp.data.remote.dto.CoinDto
+import retrofit2.http.GET
+import retrofit2.http.Path
+
+interface CoinPaprikaApi {
+    @GET("/v1/coins")
+    suspend fun getCoins(): List<CoinDto>
+
+    @GET("/v1/coins/{coinId}")
+    suspend fun getCoinById(@Path("coinId") coinId: String): CoinDto
+}
---
+++ app/src/main/java/com/example/cryptoapp/data/remote/dto/CoinDto.kt
@@ -0,0 +1,32 @@
+package com.example.cryptoapp.data.remote.dto
+
+
+import com.example.cryptoapp.domain.model.Coin
+import com.google.gson.annotations.SerializedName
+
+data class CoinDto(
+    @SerializedName("id")
+    val id: String,
+    @SerializedName("is_active")
+    val isActive: Boolean,
+    @SerializedName("is_new")
+    val isNew: Boolean,
+    @SerializedName("name")
+    val name: String,
+    @SerializedName("rank")
+    val rank: Int,
+    @SerializedName("symbol")
+    val symbol: String,
+    @SerializedName("type")
+    val type: String
+)
+
+fun CoinDto.toCoin(): Coin {
+    return Coin(
+        id,
+        isActive,
+        name,
+        rank,
+        symbol
+    )
+}
